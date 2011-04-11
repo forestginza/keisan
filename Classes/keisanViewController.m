f745d96fcc4ddce8b40ef40c256186f23b1087d1
tei <dingyoubing@gmail.com>
2011-04-11 20:31:28 +0900

    ddd

--- Classes/keisanViewController.m
+++ Classes/keisanViewController.m
@@ -46,6 +46,13 @@
 - (void)didReceiveMemoryWarning {
     // Releases the view if it doesn't have a superview.
     [super didReceiveMemoryWarning];
+    //aaaaaaaaaaaa
+    
+    
+    //bbbbbbbbbbbbbbbbbb
+    
+    
+    
     
     // Release any cached data, images, etc. that aren't in use.
 }
